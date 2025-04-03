89c4110b3efb1884bde4cdb4972e98adaef78ee6
gamblor21 <mark.komus@gmail.com>
2025-04-02 19:48:59 -0500

    Fixed copyright year oops

--- shared-bindings/audiodelays/Reverb.h
+++ shared-bindings/audiodelays/Reverb.h
@@ -1,6 +1,6 @@
 // This file is part of the CircuitPython project: https://circuitpython.org
 //
-// SPDX-FileCopyrightText: Copyright (c) 2024 Mark Komus
+// SPDX-FileCopyrightText: Copyright (c) 2025 Mark Komus
 //
 // SPDX-License-Identifier: MIT
 
