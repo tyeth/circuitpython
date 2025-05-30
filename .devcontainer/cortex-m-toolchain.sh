84a73bf6806403dcad7490f143153a2f2774618e
Dan Halbert <halbert@halwitz.org>
2025-05-30 10:33:09 -0400

    .devcontainer/cortex-m-toolchain.sh: update to gcc14

--- .devcontainer/cortex-m-toolchain.sh
+++ .devcontainer/cortex-m-toolchain.sh
@@ -14,10 +14,10 @@ echo -e "[cortex-m-toolchain.sh] downloading and installing gcc-arm-non-eabi too
 cd /workspaces
 
 wget -qO gcc-arm-none-eabi.tar.xz \
-  https://developer.arm.com/-/media/Files/downloads/gnu/13.2.rel1/binrel/arm-gnu-toolchain-13.2.rel1-x86_64-arm-none-eabi.tar.xz
+  https://developer.arm.com/-/media/Files/downloads/gnu/13.2.rel1/binrel/arm-gnu-toolchain-14.2.rel1-x86_64-arm-none-eabi.tar.xz
 
 tar -xJf gcc-arm-none-eabi.tar.xz
-ln -s arm-gnu-toolchain-13.2.Rel1-x86_64-arm-none-eabi gcc-arm-none-eabi
+ln -s arm-gnu-toolchain-14.2.Rel1-x86_64-arm-none-eabi gcc-arm-none-eabi
 rm -f gcc-arm-none-eabi.tar.xz
 
 echo -e "[cortex-m-toolchain.sh] update PATH in environment"
