24eca69860aef07d333967f2b94fc1812fff5053
Tyeth Gundry <tyethgundry@googlemail.com>
2026-02-15 01:36:02 +0000

    add(board): Super Mini ESP32-S3

---
+++ ports/espressif/boards/super_mini_esp32_s3/mpconfigboard.h
@@ -0,0 +1,18 @@
+// This file is part of the CircuitPython project: https://circuitpython.org
+//
+// SPDX-FileCopyrightText: Copyright (c) 2023 David Sullivan
+//
+// SPDX-License-Identifier: MIT
+
+#pragma once
+
+// Micropython setup
+
+#define MICROPY_HW_BOARD_NAME       "Super Mini ESP32-S3"
+#define MICROPY_HW_MCU_NAME         "ESP32S3"
+
+#define MICROPY_HW_NEOPIXEL_ORDER_GRB (1)
+#define MICROPY_HW_NEOPIXEL (&pin_GPIO48)
+
+#define DEFAULT_UART_BUS_RX (&pin_GPIO44)
+#define DEFAULT_UART_BUS_TX (&pin_GPIO43)
